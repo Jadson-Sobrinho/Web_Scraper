b97dcae37bc7c8e8e3db109b783e8fed792be6b1
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-03-24 14:59:51 -0300

    - Puppeteer config

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
+(async () => {
+    
+    /*Tirar a abertura do navegador e o slowmotion quando enviar*/
+    const browser = await puppeteer.launch({ headless: false, slowMo: 100 });
+    const page = await browser.newPage();
+
+    
+    await page.goto('https://mercado.carrefour.com.br/bebidas', { waitUntil: 'load' });
+    await page.waitForSelector('[data-product-card-content="true"]', { timeout: 60000 });
+    await browser.close();
+
+    
+})();
