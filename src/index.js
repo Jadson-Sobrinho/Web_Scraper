f727e395491d97e70941ea9f273a2d8d4a398648
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-03-24 20:37:32 -0300

    Refact

--- src/index.js
+++ src/index.js
@@ -1,37 +1,45 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-(async () => {
+async function ScrapeCarrefourDrinks(){
     
     /*Tirar a abertura do navegador e o slowmotion quando enviar*/
     const browser = await puppeteer.launch({ headless: false, slowMo: 100 });
     const page = await browser.newPage();
 
-    
-    await page.goto('https://mercado.carrefour.com.br/bebidas', { waitUntil: 'load' });
-    await page.waitForSelector('[data-product-card-content="true"]', { timeout: 60000 });
+    try {
 
- 
-    const products = await page.evaluate(() => {
+        await page.goto('https://mercado.carrefour.com.br/bebidas', { waitUntil: 'load' });
+        await page.waitForSelector('[data-product-card-content="true"]', { timeout: 60000 });
 
-        const productsList = document.querySelectorAll('[data-product-card-content="true"]');
     
-        return Array.from(productsList).map(product => {
-            return {
-                nome: product.querySelector('[data-testid="product-link"]')?.innerText.trim() || 'Nome não encontrado',
-                preco: product.querySelector('[data-test-id="price"]')?.innerText.trim() || 'Preço não encontrado',
-                link: product.querySelector('[data-testid="product-link"]')?.href || 'Link não encontrado'
-            };
+        const products = await page.evaluate(() => {
+
+            const productsList = document.querySelectorAll('[data-product-card-content="true"]');
+        
+            return Array.from(productsList).map(product => {
+                return {
+                    nome: product.querySelector('[data-testid="product-link"]')?.innerText.trim() || 'Nome não encontrado',
+                    preco: product.querySelector('[data-test-id="price"]')?.innerText.trim() || 'Preço não encontrado',
+                    link: product.querySelector('[data-testid="product-link"]')?.href || 'Link não encontrado'
+                };
+            });
         });
-    });
 
     
-    fs.writeFileSync('./src/output.json', JSON.stringify(products, null, 2));
+        fs.writeFileSync('./src/output.json', JSON.stringify(products, null, 2));
+        //console.log(products);
+        console.log("ok");
+    } catch(e) {
 
-    console.log('Produtos encontrados:', products);
+        console.log("Erro", e);
 
-    await browser.close();
+    } finally{
 
+        await browser.close();
+    }
     
-})();
+};
+
+ScrapeCarrefourDrinks();
 
