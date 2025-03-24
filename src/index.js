ad4b07b2c72c9a7376a1ee849bd648fb4bb016ff
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-03-24 18:53:40 -0300

    Extract info from page

--- src/index.js
+++ src/index.js
@@ -10,7 +10,26 @@ const fs = require('fs');
     
     await page.goto('https://mercado.carrefour.com.br/bebidas', { waitUntil: 'load' });
     await page.waitForSelector('[data-product-card-content="true"]', { timeout: 60000 });
+
+ 
+    const products = await page.evaluate(() => {
+
+        const productsList = document.querySelectorAll('[data-product-card-content="true"]');
+    
+        return Array.from(productsList).map(product => {
+            return {
+                nome: product.querySelector('[data-testid="product-link"]')?.innerText.trim() || 'Nome não encontrado',
+                preco: product.querySelector('[data-test-id="price"]')?.innerText.trim() || 'Preço não encontrado',
+                link: product.querySelector('[data-testid="product-link"]')?.href || 'Link não encontrado'
+            };
+        });
+    });
+
+  
+    console.log('Produtos encontrados:', products);
+
     await browser.close();
 
     
 })();
+
